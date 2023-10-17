3bcbc6c247c5b954d23dce16678b60c843d24625
Talha Can <rampesna@gmail.com>
2023-10-17 15:41:46 +0300

    v1.1.0

--- src/JqxGrid.php
+++ src/JqxGrid.php
@@ -8,16 +8,24 @@ use Illuminate\Support\Facades\DB;
 class JqxGrid
 {
     private $tableName;
-
     private $columns;
-
     private $request;
+    private $filterConditions = [
+        'CONTAINS' => ['like', '%VALUE%'],
+        'DOES_NOT_CONTAIN' => ['not like', '%VALUE%'],
+        'EQUAL' => ['=', 'VALUE'],
+        'NOT_EQUAL' => ['<>', 'VALUE'],
+        'GREATER_THAN' => ['>', 'VALUE'],
+        'LESS_THAN' => ['<', 'VALUE'],
+        'GREATER_THAN_OR_EQUAL' => ['>=', 'VALUE'],
+        'LESS_THAN_OR_EQUAL' => ['<=', 'VALUE'],
+        'STARTS_WITH' => ['like', 'VALUE%'],
+        'ENDS_WITH' => ['like', '%VALUE'],
+        'NULL' => ['is null', null],
+        'NOT_NULL' => ['is not null', null],
+    ];
 
-    public function __construct(
-        string  $tableName,
-        array   $columns,
-        Request $request
-    )
+    public function __construct(string $tableName, array $columns, Request $request)
     {
         $this->tableName = $tableName;
         $this->columns = $columns;
@@ -26,111 +34,51 @@ class JqxGrid
 
     public function initialize()
     {
-        $pageNum = $this->request->pagenum;
-        $pageSize = $this->request->pagesize;
-        $start = $pageNum * $pageSize;
+        $start = $this->request->pagenum * $this->request->pagesize;
+        $conditions = $this->buildFilterConditions();
 
-        $filtersCount = $this->request->filterscount ?? 0;
-        $conditions = [];
+        $query = DB::table($this->tableName)->select($this->columns);
 
-        for ($i = 0; $i < $filtersCount; $i++) {
-            $filterValue = $this->request->input("filtervalue{$i}");
-            $filterCondition = $this->request->input("filtercondition{$i}");
-            $filterDataField = $this->request->input("filterdatafield{$i}");
-            $filterOperator = $this->request->input("filteroperator{$i}");
-
-            $condition = '';
-            $value = '';
-
-            switch ($filterCondition) {
-                case 'CONTAINS':
-                    $condition = 'like';
-                    $value = "%{$filterValue}%";
-                    break;
-                case 'DOES_NOT_CONTAIN':
-                    $condition = 'not like';
-                    $value = "%{$filterValue}%";
-                    break;
-                case 'EQUAL':
-                    $condition = '=';
-                    $value = $filterValue;
-                    break;
-                case 'NOT_EQUAL':
-                    $condition = '<>';
-                    $value = $filterValue;
-                    break;
-                case 'GREATER_THAN':
-                    $condition = '>';
-                    $value = $filterValue;
-                    break;
-                case 'LESS_THAN':
-                    $condition = '<';
-                    $value = $filterValue;
-                    break;
-                case 'GREATER_THAN_OR_EQUAL':
-                    $condition = '>=';
-                    $value = $filterValue;
-                    break;
-                case 'LESS_THAN_OR_EQUAL':
-                    $condition = '<=';
-                    $value = $filterValue;
-                    break;
-                case 'STARTS_WITH':
-                    $condition = 'like';
-                    $value = "{$filterValue}%";
-                    break;
-                case 'ENDS_WITH':
-                    $condition = 'like';
-                    $value = "%{$filterValue}";
-                    break;
-                case 'NULL':
-                    $condition = 'is null';
-                    break;
-                case 'NOT_NULL':
-                    $condition = 'is not null';
-                    break;
-            }
-
-            $conditions[] = [
-                'column' => $filterDataField,
-                'operator' => $condition,
-                'value' => $value,
-                'boolean' => ($i > 0) ? $filterOperator === 0 ? 'and' : 'or' : '',
-            ];
-        }
-
-        $query = DB::table(
-            $this->tableName
-        )->select(
-            $this->columns
-        );
-
-        if (
-            isset($this->request->sortdatafield) &&
-            $this->request->sortorder
-        ) {
-            $sortField = $this->request->sortdatafield;
-            $sortOrder = $this->request->sortorder;
-
-            if ($sortField) {
-                if ($sortOrder == "desc") {
-                    $query->orderByDesc($sortField);
-                } else if ($sortOrder == "asc") {
-                    $query->orderBy($sortField);
-                }
-            }
+        if ($sortField = $this->request->sortdatafield) {
+            $query->orderBy($sortField, $this->request->sortorder ?? 'asc');
         }
 
         foreach ($conditions as $condition) {
             $query->where($condition['column'], $condition['operator'], $condition['value'], $condition['boolean']);
         }
 
-        $totalRows = $query->count();
-        $rows = $query->skip($start)->take($pageSize)->get();
-
         return [
-            'TotalRows' => $totalRows,
-            'Rows' => $rows,
+            'TotalRows' => $query->count(),
+            'Rows' => $query->skip($start)->take($this->request->pagesize)->get(),
         ];
+    }
+
+    private function buildFilterConditions(): array
+    {
+        $filtersCount = $this->request->filterscount ?? 0;
+        $conditions = [];
+
+        for ($i = 0; $i < $filtersCount; $i++) {
+            $filterCondition = $this->request->input("filtercondition{$i}");
+
+            if (!isset($this->filterConditions[$filterCondition])) {
+                continue;
+            }
+
+            [$condition, $value] = $this->filterConditions[$filterCondition];
+
+            if ($value !== null) {
+                $value = str_replace('VALUE', $this->request->input("filtervalue{$i}"), $value);
+            }
+
+            $conditions[] = [
+                'column' => $this->request->input("filterdatafield{$i}"),
+                'operator' => $condition,
+                'value' => $value,
+                'boolean' => ($i > 0) ? $this->request->input("filteroperator{$i}") === 0 ? 'and' : 'or' : '',
+            ];
+        }
+
+        return $conditions;
     }
 }
